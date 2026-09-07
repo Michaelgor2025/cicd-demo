bd9a78f655dadf65439789eccb457da56c011dbd
Michael <>
2026-09-07 10:04:18 +0800

    Initial project

---
+++ cicd-demo.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace cicd_demo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        { 
+            // Arrange
+            int a = 10; 
+            int b = 20; 
+
+            // Act
+            int result = a + b;
+
+            //Assert
+            Assert.Equal(30, result); 
+        }
+    }
+}
