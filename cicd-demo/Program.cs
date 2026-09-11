614488223ccfb752cda691dcfda4caad5d1e367f
michael_Lu <124236436@qq.com>
2026-09-11 21:26:28 +0800

    feat: update api version

--- cicd-demo/Program.cs
+++ cicd-demo/Program.cs
@@ -15,15 +15,16 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    //var forecast = Enumerable.Range(1, 5).Select(index =>
+    //    new WeatherForecast
+    //    (
+    //        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //        Random.Shared.Next(-20, 55),
+    //        summaries[Random.Shared.Next(summaries.Length)]
+    //    ))
+    //    .ToArray();
+    //return forecast;
+    return "Hello from CI/CD - Version 2";
 });
 
 app.Run();
