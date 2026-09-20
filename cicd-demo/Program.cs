10ea15ec2999d4f91d9c8df6f7bac9f8214eb729
michael_Lu <124236436@qq.com>
2026-09-20 09:46:28 +0800

    test production rollback failure5

--- cicd-demo/Program.cs
+++ cicd-demo/Program.cs
@@ -20,7 +20,7 @@ app.MapGet("/health", (HttpContext context) =>
 {
     //var port = context.Connection.LocalPort;
 
-    return "Hello from CI/CD - Version 13";
+    return "Hello from CI/CD - Version 14";
     //throw new Exception("Internal Server Error");
 });
 
